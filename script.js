77c9bfeb29d7c81778dd6c95964846fcd609cfb8
Saurabhsahuji <saurabhsahu921940@gmail.com>
2025-04-21 21:38:48 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+function showAlert(action) {
+    alert(`${action} functionality is not implemented in this demo.`);
+}
+
+// Navbar scroll effect
+window.addEventListener('scroll', () => {
+    const navbar = document.querySelector('.navbar');
+    if (window.scrollY > 50) {
+        navbar.classList.add('scrolled');
+    } else {
+        navbar.classList.remove('scrolled');
+    }
+});
